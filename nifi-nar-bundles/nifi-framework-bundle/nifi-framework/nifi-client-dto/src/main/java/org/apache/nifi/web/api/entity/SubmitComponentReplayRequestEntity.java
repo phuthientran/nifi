c252bc21de69db1e12b5d8934bb142a49c1b5b89
Jennifer Bruce <jenniferbruce@zapatatechnology.com>
2020-06-24 20:48:24 -0400

    Fix formatting replace tabs with spaces

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-client-dto/src/main/java/org/apache/nifi/web/api/entity/SubmitComponentReplayRequestEntity.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-client-dto/src/main/java/org/apache/nifi/web/api/entity/SubmitComponentReplayRequestEntity.java
@@ -12,59 +12,62 @@ import org.apache.nifi.web.api.dto.util.DateTimeAdapter;
  * A serialized representation of this class can be placed in the entity body of
  * a request to the API to replay items from the specified component that were
  * dropped within the provided date range.
- * 
- * The clusterNodeId can remain null if the NiFi instance is not running in a cluster.
+ *
+ * The clusterNodeId can remain null if the NiFi instance is not running in a
+ * cluster.
  */
+
 @XmlRootElement(name = "componentReplayRequestEntity")
 public class SubmitComponentReplayRequestEntity {
 
     private String componentId;
+
     private String clusterNodeId;
     private Date startDate;
     private Date endDate;
     private Integer maxResults;
 
-	public String getComponentId() {
-		return componentId;
-	}
+    public String getComponentId() {
+        return componentId;
+    }
 
-	public void setComponentId(String queueId) {
-		this.componentId = queueId;
-	}
+    public void setComponentId(String queueId) {
+        this.componentId = queueId;
+    }
 
-	public String getClusterNodeId() {
-		return clusterNodeId;
-	}
+    public String getClusterNodeId() {
+        return clusterNodeId;
+    }
 
-	public void setClusterNodeId(String clusterNodeId) {
-		this.clusterNodeId = clusterNodeId;
-	}
+    public void setClusterNodeId(String clusterNodeId) {
+        this.clusterNodeId = clusterNodeId;
+    }
 
-	@XmlJavaTypeAdapter(DateTimeAdapter.class)
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern=DateTimeAdapter.DEFAULT_DATE_TIME_FORMAT)
-	public Date getStartDate() {
-		return startDate;
-	}
+    @XmlJavaTypeAdapter(DateTimeAdapter.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateTimeAdapter.DEFAULT_DATE_TIME_FORMAT)
+    public Date getStartDate() {
+        return startDate;
+    }
 
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
 
-	@XmlJavaTypeAdapter(DateTimeAdapter.class)
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern=DateTimeAdapter.DEFAULT_DATE_TIME_FORMAT)
-	public Date getEndDate() {
-		return endDate;
-	}
+    @XmlJavaTypeAdapter(DateTimeAdapter.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateTimeAdapter.DEFAULT_DATE_TIME_FORMAT)
+    public Date getEndDate() {
+        return endDate;
+    }
 
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 
-	public Integer getMaxResults() {
-		return maxResults;
-	}
+    public Integer getMaxResults() {
+        return maxResults;
+    }
 
-	public void setMaxResults(Integer maxResults) {
-		this.maxResults = maxResults;
-	}
+    public void setMaxResults(Integer maxResults) {
+        this.maxResults = maxResults;
+    }
 }
