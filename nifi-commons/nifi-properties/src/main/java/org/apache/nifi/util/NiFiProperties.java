d426320dfb72c823f87b647a3445717f6d4a859c
Phu-Thien Tran <phuthientran@zapatatechnology.com>
2020-07-21 22:19:53 -1000

    Add nifi.web.context.root to support prefixing "/nifi", and other webapp contexts with a context root to work with mod_proxy.

--- nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
+++ nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
@@ -208,10 +208,15 @@ public abstract class NiFiProperties {
     public static final String WEB_MAX_HEADER_SIZE = "nifi.web.max.header.size";
     public static final String WEB_PROXY_CONTEXT_PATH = "nifi.web.proxy.context.path";
     public static final String WEB_PROXY_HOST = "nifi.web.proxy.host";
+<<<<<<< Upstream, based on origin/master
     public static final String WEB_MAX_CONTENT_SIZE = "nifi.web.max.content.size";
     public static final String WEB_MAX_REQUESTS_PER_SECOND = "nifi.web.max.requests.per.second";
     public static final String WEB_SHOULD_SEND_SERVER_VERSION = "nifi.web.should.send.server.version";
 
+=======
+    public static final String WEB_CONTEXT_ROOT = "nifi.web.context.root";
+    
+>>>>>>> 78362f5 Add nifi.web.context.root to support prefixing "/nifi", and other webapp contexts with a context root to work with mod_proxy.
     // ui properties
     public static final String UI_BANNER_TEXT = "nifi.ui.banner.text";
     public static final String UI_AUTO_REFRESH_INTERVAL = "nifi.ui.autorefresh.interval";
@@ -1450,6 +1455,12 @@ public abstract class NiFiProperties {
 
     public Integer getFlowConfigurationArchiveMaxCount() {
         return getIntegerProperty(FLOW_CONFIGURATION_ARCHIVE_MAX_COUNT, null);
+    }
+    
+    public String getWebContextRoot() {
+        String context = getProperty(WEB_CONTEXT_ROOT, "");
+        // Force prefixing with "/"
+        return context.replaceAll("/?(.+)", "/$1");
     }
 
     public String getVariableRegistryProperties() {
