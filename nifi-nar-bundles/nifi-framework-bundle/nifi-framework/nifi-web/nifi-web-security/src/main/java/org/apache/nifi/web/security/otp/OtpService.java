b3e7e341412ad5366c9ee68d3d9913608f53aa55
phuthientran <58049725+phuthientran@users.noreply.github.com>
2021-01-08 16:59:04 -1000

    Revert "NIFI-7385 Provided reverse-indexed TokenCache implementation. Cleaned up code style. Unit test was failing on Windows 1.8 GitHub Actions build but no other environment. Increased artificial delay to avoid timing issues."
    This reverts commit e14e46d97b55be03e13550756eca904104b7fc57.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-security/src/main/java/org/apache/nifi/web/security/otp/OtpService.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-security/src/main/java/org/apache/nifi/web/security/otp/OtpService.java
@@ -16,18 +16,22 @@
  */
 package org.apache.nifi.web.security.otp;
 
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.concurrent.TimeUnit;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.nifi.web.security.token.OtpAuthenticationToken;
 import org.apache.nifi.web.security.util.CacheKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * OtpService is a service for generating and verifying one time password tokens.
@@ -41,8 +45,8 @@ public class OtpService {
     // protected for testing purposes
     protected static final int MAX_CACHE_SOFT_LIMIT = 100;
 
-    private final TokenCache downloadTokens;
-    private final TokenCache uiExtensionTokens;
+    private final Cache<CacheKey, String> downloadTokenCache;
+    private final Cache<CacheKey, String> uiExtensionCache;
 
     /**
      * Creates a new OtpService with an expiration of 5 minutes.
@@ -60,8 +64,8 @@ public class OtpService {
      * @throws IllegalArgumentException If duration is negative
      */
     public OtpService(final int duration, final TimeUnit units) {
-        downloadTokens = new TokenCache("download tokens", duration, units);
-        uiExtensionTokens = new TokenCache("UI extension tokens", duration, units);
+        downloadTokenCache = CacheBuilder.newBuilder().expireAfterWrite(duration, units).build();
+        uiExtensionCache = CacheBuilder.newBuilder().expireAfterWrite(duration, units).build();
     }
 
     /**
@@ -71,7 +75,7 @@ public class OtpService {
      * @return                          The one time use download token
      */
     public String generateDownloadToken(final OtpAuthenticationToken authenticationToken) {
-        return generateToken(downloadTokens, authenticationToken);
+        return generateToken(downloadTokenCache.asMap(), authenticationToken);
     }
 
     /**
@@ -82,7 +86,7 @@ public class OtpService {
      * @throws OtpAuthenticationException   When the specified token does not correspond to an authenticated identity
      */
     public String getAuthenticationFromDownloadToken(final String token) throws OtpAuthenticationException {
-        return getAuthenticationFromToken(downloadTokens, token);
+        return getAuthenticationFromToken(downloadTokenCache.asMap(), token);
     }
 
     /**
@@ -92,7 +96,7 @@ public class OtpService {
      * @return                          The one time use UI extension token
      */
     public String generateUiExtensionToken(final OtpAuthenticationToken authenticationToken) {
-        return generateToken(uiExtensionTokens, authenticationToken);
+        return generateToken(uiExtensionCache.asMap(), authenticationToken);
     }
 
     /**
@@ -103,55 +107,42 @@ public class OtpService {
      * @throws OtpAuthenticationException   When the specified token does not correspond to an authenticated identity
      */
     public String getAuthenticationFromUiExtensionToken(final String token) throws OtpAuthenticationException {
-        return getAuthenticationFromToken(uiExtensionTokens, token);
+        return getAuthenticationFromToken(uiExtensionCache.asMap(), token);
     }
 
     /**
      * Generates a token and stores it in the specified cache.
      *
-     * @param tokenCache                A cache that maps tokens to users
+     * @param cache                     The cache
      * @param authenticationToken       The authentication
      * @return                          The one time use token
      */
-    private String generateToken(final TokenCache tokenCache, final OtpAuthenticationToken authenticationToken) {
-        final String userId = (String) authenticationToken.getPrincipal();
-
-        // If the user has a token already, return it
-        if(tokenCache.containsValue(userId)) {
-            return (tokenCache.getKeyForValue(userId)).getKey();
-        } else {
-            // Otherwise, generate a token
-            if (tokenCache.size() >= MAX_CACHE_SOFT_LIMIT) {
-                throw new IllegalStateException("The maximum number of single use tokens have been issued.");
-            }
-
-            // Hash the authentication and build a cache key
-            final CacheKey cacheKey = new CacheKey(hash(authenticationToken));
-
-            // Store the token and user in the cache
-            tokenCache.put(cacheKey, userId);
-
-            // Return the token
-            return cacheKey.getKey();
+    private String generateToken(final ConcurrentMap<CacheKey, String> cache, final OtpAuthenticationToken authenticationToken) {
+        if (cache.size() >= MAX_CACHE_SOFT_LIMIT) {
+            throw new IllegalStateException("The maximum number of single use tokens have been issued.");
         }
+
+        // hash the authentication and build a cache key
+        final CacheKey cacheKey = new CacheKey(hash(authenticationToken));
+
+        // store the token unless the token is already stored which should not update it's original timestamp
+        cache.putIfAbsent(cacheKey, authenticationToken.getName());
+
+        // return the token
+        return cacheKey.getKey();
     }
 
     /**
-     * Gets the corresponding authentication for the specified one time use token. The specified token will be removed
-     * from the token cache.
+     * Gets the corresponding authentication for the specified one time use token. The specified token will be removed.
      *
-     * @param tokenCache                A cache that maps tokens to users
+     * @param cache                     The cache
      * @param token                     The one time use token
      * @return                          The authenticated identity
      */
-    private String getAuthenticationFromToken(final TokenCache tokenCache, final String token) throws OtpAuthenticationException {
-        final CacheKey cacheKey = new CacheKey(token);
-        final String authenticatedUser = (String) tokenCache.getIfPresent(cacheKey);
-
+    private String getAuthenticationFromToken(final ConcurrentMap<CacheKey, String> cache, final String token) throws OtpAuthenticationException {
+        final String authenticatedUser = cache.remove(new CacheKey(token));
         if (authenticatedUser == null) {
             throw new OtpAuthenticationException("Unable to validate the access token.");
-        } else {
-            tokenCache.invalidate(cacheKey);
         }
 
         return authenticatedUser;
